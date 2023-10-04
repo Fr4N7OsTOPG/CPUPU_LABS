97a3bb4a1cc1dc113167040799b5827a9576dc02
Damian <114771993+Fr4N7OsTOPG@users.noreply.github.com>
2023-10-04 17:16:00 +0300

    Update lab2.h

--- LAB2/lab2.hpp
+++ LAB2/lab2.hpp
@@ -1,1 +1,41 @@
+#ifndef LAB2_LAB2_H
+#define LAB2_LAB2_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <exception>
+using namespace std;
+class Methods{
+private:
+    fstream file;
+public:
+    WorkwFiles(){};
 
+    ~WorkwFiles() {
+    }
+    void open(string name) {
+        file.open(name, std::ios::app);
+        if(...)
+        throw std::exception();
+    }
+    void close(){
+        file.close();
+    }
+    bool isOpen(){
+        return file.is_open();
+    }
+    string ReadLine{
+        string line;
+        getline(file, line);
+        return line;
+    }
+    string ReadWord() {
+        string word;
+        file >> word;
+        return word;
+    }
+    
+};
+
+
+#endi
