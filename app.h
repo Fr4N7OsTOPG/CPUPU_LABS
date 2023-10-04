c46421fd468e5f0e554ddaf869f2b8f176edabd6
Bulat <114771993+Fr4N7OsTOPG@users.noreply.github.com>
2023-10-04 21:10:15 +0300

    Update app.h

--- app.h
+++ app.h
@@ -1,1 +1,13 @@
+#ifndef CPUPU_LABS_APP_H
+#define CPUPU_LABS_APP_H
+#include "Matrix.cpp"
+#include "Swap.cpp"
+#include "Console.cpp"
 
+class App: Console {
+public:
+    int start();
+};
+
+
+#endif
