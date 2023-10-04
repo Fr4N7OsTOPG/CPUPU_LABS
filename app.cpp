8329a82e1905eab3e54fa26af343ab68dfe5b6de
Bulat <114771993+Fr4N7OsTOPG@users.noreply.github.com>
2023-10-04 21:12:43 +0300

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -1,1 +1,14 @@
-
+#include "app.h"
+int App::start() {
+    int x = 4;
+    double y = 1.6;
+    print(swap(x, 3));
+    print(swap(y, 3));
+    Stack s(2);
+    s.push(4);
+    s.push(12);
+    s.pop();
+    s.pop();
+    s.pop();
+    return 0;
+}
