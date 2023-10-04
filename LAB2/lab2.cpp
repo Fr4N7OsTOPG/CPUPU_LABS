b31794854559cddf6e7c2c0cb8f5717ae639c435
Damian <114771993+Fr4N7OsTOPG@users.noreply.github.com>
2023-10-04 17:08:10 +0300

    Update lab2.cpp

--- LAB2/lab2.cpp
+++ LAB2/lab2.cpp
@@ -1,1 +1,16 @@
-le code
+#include <iostream>
+#include "WorkwFiles.h"
+int main() {
+    auto file = new WorkwFiles();
+    try {
+        file->open();
+    } catch (exception &e) {
+        cout << "opening file error" << e.what() << endl;
+    }
+    string nameoffile;
+    cin >> nameoffile;
+    file->open(nameoffile);
+    cout << file->isOpen();
+    file->readstr(s);
+    file->readword(num);
+}
