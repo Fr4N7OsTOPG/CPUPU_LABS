79ad59471540dd67afeae1f86c8824a27a3f8672
Bulat <114771993+Fr4N7OsTOPG@users.noreply.github.com>
2023-10-04 18:32:48 +0300

    Create circle.cpp

---
+++ circle.cpp
@@ -0,0 +1,10 @@
+#include "circle.h"
+#include ""
+float Square(){
+    square_circle =  a_circle * a_circle * 3.14159;
+    return square_circle;
+};
+float perimeter(){
+    per_cicrcle = 6.28318 * a_circle;
+    return per_circle;
+};
