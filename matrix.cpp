309f28a9bdee33f77624bffaeaa6d4ad48bcba19
Bulat <114771993+Fr4N7OsTOPG@users.noreply.github.com>
2023-10-05 16:17:07 +0300

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,1 +1,37 @@
+#include "matrix.h"
+#include <iostream>
 
+using namespace std;
+
+Matrix::Matrix(int rows, int cols) : numRows(rows), numCols(cols) {
+    data = new int*[numRows];
+    for (int i = 0; i < numRows; ++i) {
+        data[i] = new int[numCols];
+    }
+}
+
+Matrix::~Matrix() {
+    for (int i = 0; i < numRows; ++i) {
+        delete[] data[i];
+    }
+    delete[] data;
+}
+
+void Matrix::fillMatrix() {
+    for (int i = 0; i < numRows; ++i) {
+        for (int j = 0; j < numCols; ++j) {
+            cout << "Введите элемент [" << i << "][" << j << "]: ";
+            cin >> data[i][j];
+        }
+    }
+}
+
+void Matrix::printMatrix() {
+    cout << "Матрица:" << endl;
+    for (int i = 0; i < numRows; ++i) {
+        for (int j = 0; j < numCols; ++j) {
+            cout << data[i][j] << ' ';
+        }
+        cout << endl;
+    }
+}
