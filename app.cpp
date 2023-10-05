a7fa3e62a7831c1c8820851e0bf99976e45e3a82
Bulat <114771993+Fr4N7OsTOPG@users.noreply.github.com>
2023-10-05 15:56:17 +0300

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -1,9 +1,8 @@
 #include "app.h"
 int App::start() {
-    int x = 4;
-    double y = 1.6;
-    print(swap(x, 3));
-    print(swap(y, 3));
+    char x = 4;
+    char y = 1.6;
+    print(swap(x, y))
     Stack s(2);
     s.push(4);
     s.push(12);
